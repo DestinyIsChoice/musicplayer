85927bfcefb643918ba511b441a1e5d082bb33b8
DestinyIsChoice <114787539+DestinyIsChoice@users.noreply.github.com>
2025-08-07 18:06:42 -0400

    Add files via upload

---
+++ musicstartup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install dependencies.
+yes | pkg i python
+yes | pkg i x11-repo
+yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
+yes | pkg i xorgproto
+yes | pkg i ffmpeg
+pip install inputimeout
+pip install mutagen
+pip install pygame
+pip install youtube-search
+pip install yt_dlp
+
+# Stop operating system from closing Termux.
+termux-wake-lock
+
+# Patch PulseAudio in order to allow for audio to be played.
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
+cp "$SCRIPT_DIR/default.pa" /data/data/com.termux/files/usr/etc/pulse/
+
+# Clear console and run music player.
+clear
+python "$SCRIPT_DIR/musicplayer.py"
