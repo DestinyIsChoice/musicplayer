389232a83a9c810e8948021d7fabd966129146d3
DestinyIsChoice <114787539+DestinyIsChoice@users.noreply.github.com>
2025-08-26 22:01:35 -0400

    Add files via upload

--- musicstartup.sh
+++ musicstartup.sh
@@ -1,27 +1,45 @@
 #!/bin/bash
 
-# Install dependencies.
-yes | pkg i python
-yes | pkg i x11-repo
-yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
-yes | pkg i xorgproto
-yes | pkg i ffmpeg
-pip install inputimeout
-pip install mutagen
-pip install pygame
-pip install pytubefix
-pip install youtube-search
-pip install yt_dlp
+# Detect operating system.
+if [ $(ps -ef|grep -c com.termux ) -gt 0 ]; then
 
-# Stop operating system from closing Termux.
-termux-wake-lock
+    # Install dependencies.
+    yes | pkg i python
+    yes | pkg i x11-repo
+    yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
+    yes | pkg i xorgproto
+    yes | pkg i ffmpeg
+    pip install inputimeout
+    pip install mutagen
+    pip install pygame
+    pip install pytube
+    pip install pytubefix
+    pip install youtube-search
+    pip install yt_dlp
 
-# Patch PulseAudio in order to allow for audio to be played.
-SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
-rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
-cp "$SCRIPT_DIR/default.pa" /data/data/com.termux/files/usr/etc/pulse/
+    # Stop operating system from closing Termux.
+    termux-wake-lock
 
-# Clear console and run music player.
-cd $SCRIPT_DIR
-clear
-python "$SCRIPT_DIR/musicplayer.py"
+    # Patch PulseAudio in order to allow for audio to be played.
+    SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+    rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
+    cp "$SCRIPT_DIR/default.pa" /data/data/com.termux/files/usr/etc/pulse/
+
+    # Clear console and run music player.
+    cd $SCRIPT_DIR
+    clear
+    python "$SCRIPT_DIR/musicplayer.py"
+else
+
+    # Install dependencies.
+    pip install inputimeout
+    pip install mutagen
+    pip install pygame
+    pip install pytubefix
+    pip install youtube-search
+    pip install yt_dlp
+
+    # Clear console and run music player.
+    clear
+    python musicplayer/musicplayer.py
+fi
