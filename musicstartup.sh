32b94a626261748f5ec045d77fb526b2a58fcb5d
DestinyIsChoice <114787539+DestinyIsChoice@users.noreply.github.com>
2025-09-02 11:12:21 -0400

    Add files via upload

--- musicstartup.sh
+++ musicstartup.sh
@@ -17,20 +17,15 @@ if echo "$PREFIX" | grep -q "com.termux"; then
     # Patch PulseAudio in order to allow for audio to be played.
     rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
     cp "musicplayer/default.pa" /data/data/com.termux/files/usr/etc/pulse/
-
-    # Clear console and run music player.
-    cd musicplayer || return
-    clear
-    export PYGAME_HIDE_SUPPORT_PROMPT="1"
-    python musicplayer.py
 else
 
     # Install dependencies.
     pip install -r musicplayer/requirements.txt
+fi
 
     # Clear console and run music player.
     cd musicplayer || return
+    git pull origin main
     clear
     export PYGAME_HIDE_SUPPORT_PROMPT="1"
     python musicplayer.py
-fi
