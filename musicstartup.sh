d36640169f88b856d524a678c092a4fab63f73b0
DestinyIsChoice <114787539+DestinyIsChoice@users.noreply.github.com>
2025-09-01 21:13:16 -0400

    Add files via upload

--- musicstartup.sh
+++ musicstartup.sh
@@ -1,36 +1,25 @@
 #!/bin/bash
 
-# Detect operating system.
-if echo "$PREFIX" | grep -q "com.termux"; then
+# Install dependencies.
+yes | pkg i python
+yes | pkg i x11-repo
+yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
+yes | pkg i xorgproto
+yes | pkg i ffmpeg
+pip install inputimeout
+pip install mutagen
+pip install pygame
+pip install youtube-search
+pip install yt_dlp
 
-    # Install dependencies.
-    yes | pkg i python
-    yes | pkg i x11-repo
-    yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
-    yes | pkg i xorgproto
-    yes | pkg i ffmpeg
-    pip install -r musicplayer/requirements.txt
+# Stop operating system from closing Termux.
+termux-wake-lock
 
-    # Stop operating system from closing Termux.
-    termux-wake-lock
+# Patch PulseAudio in order to allow for audio to be played.
+SCRIPT_DIR=$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")
+rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
+cp "$SCRIPT_DIR/default.pa" /data/data/com.termux/files/usr/etc/pulse/
 
-    # Patch PulseAudio in order to allow for audio to be played.
-    rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
-    cp "musicplayer/default.pa" /data/data/com.termux/files/usr/etc/pulse/
-
-    # Clear console and run music player.
-    cd musicplayer || return
-    clear
-    export PYGAME_HIDE_SUPPORT_PROMPT="1"
-    python musicplayer.py
-else
-
-    # Install dependencies.
-    pip install -r musicplayer/requirements.txt
-
-    # Clear console and run music player.
-    cd musicplayer || return
-    clear
-    export PYGAME_HIDE_SUPPORT_PROMPT="1"
-    python musicplayer.py
-fi
+# Clear console and run music player.
+clear
+python "$SCRIPT_DIR/musicplayer.py"
