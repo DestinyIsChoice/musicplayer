54561b62ee361134d2fe65b8a3a17da9bf900367
Destiny <114787539+DestinyIsChoice@users.noreply.github.com>
2025-09-04 21:08:01 -0400

    Add files via upload

---
+++ musicstartup (1).sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Detect operating system.
+if echo "$PREFIX" | grep -q "com.termux"; then
+
+    # Install dependencies.
+    yes | pkg i python
+    yes | pkg i x11-repo
+    yes | pkg i sdl2 sdl2-gfx sdl2-image sdl2-mixer sdl2-ttf
+    yes | pkg i xorgproto
+    yes | pkg i ffmpeg
+    pip install -r musicplayer/requirements.txt
+
+    # Stop operating system from closing Termux.
+    termux-wake-lock
+
+    # Patch PulseAudio in order to allow for audio to be played.
+    rm -f /data/data/com.termux/files/usr/etc/pulse/default.pa
+    cp "musicplayer/default.pa" /data/data/com.termux/files/usr/etc/pulse/
+else
+
+    # Install dependencies.
+    pip install -r musicplayer/requirements.txt
+fi
+
+    # Clear console and run music player.
+    cd musicplayer || return
+    git pull origin main
+    clear
+    export PYTHONIOENCODING=utf-8
+    export PYGAME_HIDE_SUPPORT_PROMPT="1"
+    python musicplayer.py
